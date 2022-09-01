59d4bfae9c5e06e64f0e7f6a4440f63ee6fc365e
Parker Bossier <parkerbossier@gmail.com>
2022-09-01 03:00:35 -0400

    Overlay test

--- App.tsx
+++ App.tsx
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { AutoFocus, Camera, CameraType } from 'expo-camera';
+import { DeviceMotion } from 'expo-sensors';
+import { useEffect } from 'react';
+import { Image, StyleSheet, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [permission, requestPermission] = Camera.useCameraPermissions();
+
+	if (!permission?.granted)
+		requestPermission();
+
+	useEffect(
+		() => {
+			const sub = DeviceMotion.addListener(e => {
+				console.log(e.accelerationIncludingGravity);
+			});
+
+			return () => {
+				DeviceMotion.removeSubscription(sub);
+			};
+		},
+		[]
+	);
+
+
+	return (
+		<View style={styles.container}>
+			<Camera
+				autoFocus={AutoFocus.on}
+				style={styles.camera}
+				type={CameraType.back}
+			/>
+			<View style={styles.overlay}>
+				<Image
+					source={require('./assets/analemma.png')}
+				/>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#000',
+		position: 'relative'
+	},
+	camera: {
+		width: '100%',
+		height: '100%'
+	},
+	overlay: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		width: '100%',
+		height: '100%'
+	}
 });
